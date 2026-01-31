96cab881f89ecec9dbb9682cb6a30d97254a5e2b
Jgrace-sundari <gracesundari7@gmail.com>
2026-01-31 14:25:34 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+class First{
+    public static void main(String[] args){
+        System.out.print("Hello");
+        System.out.println(" World!");
+        System.out.print("Have a nice day..\n");
+        System.out.println();
+    }
+
+}
